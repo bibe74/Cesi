a7ee92b66f1dabff7eba805a182ae7b8d33774ab
Alberto Federico Turelli <alberto.turelli@gmail.com>
2025-07-28 12:25:03 +0200

    Bugfix report Cruscotto Clienti

--- Scripts/SQL/CesiDW/CesiDW_50.01_user_programmability.sql
+++ Scripts/SQL/CesiDW/CesiDW_50.01_user_programmability.sql
@@ -3459,7 +3459,7 @@ AS (
         O.PKDataCompetenza,
         O.TotaleDocumento,
         O.Insoluto,
-        ROW_NUMBER() OVER (PARTITION BY O.PKCliente ORDER BY O.PKDataInizioContratto DESC, O.NumeroDocumento DESC) AS rn
+        ROW_NUMBER() OVER (PARTITION BY O.PKCliente, O.MacroTipoAbbonamento ORDER BY O.PKDataInizioContratto DESC, O.NumeroDocumento DESC) AS rn
 
     FROM Ordini O
     WHERE O.PKDataInizioContratto <= @DataFineAnnoCorrente
@@ -3612,7 +3612,7 @@ SELECT
     IMAP.NumeroIscritti AS NumeroIscrittiAnnoPrecedente,
     IMAP.ImportoTotale AS ImportoTotaleAnnoPrecedente,
     ----@CodiceEsercizioMasterCorrente AS CodiceEsercizioCorrente,
-    CONVERT(NVARCHAR(4), @AnnoMasterPrecedente) AS CodiceEsercizioCorrente,
+    CONVERT(NVARCHAR(4), @AnnoMasterCorrente) AS CodiceEsercizioCorrente,
     IMMAP.NumeroIscritti AS NumeroIscrittiMiniMasterAnnoPrecedente,
     IMMAP.ImportoTotale AS ImportoTotaleMiniMasterAnnoPrecedente,
     IMAC.NumeroIscritti AS NumeroIscrittiAnnoCorrente,
